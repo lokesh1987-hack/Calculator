12457d52e5a9871b8bc1b01d2bb6359bfe207e7c
Liladhar harode <lokeshharode1987@gmail.com>
2021-08-04 19:56:23 +0530

    design

--- src/App.js
+++ src/App.js
@@ -4,43 +4,46 @@ import Button from 'react-bootstrap/Button'
 
 
 export default class App extends React.Component {
-  constructor(prop){
-    super(prop);  
+  constructor(prop) {
+    super(prop);
     this.state = {
-      count : 0,
-      oprend : 0,
+      count: 0,
+      oprend: 0,
     }
-}
-    
-  add = () =>{
-   this.setState({count:parseInt(this.state.count)+parseInt(this.state.oprend)})
-   this.setState({oprend : ""})
-  }
-  sub = () =>{
-    this.setState({count:(this.state.count-this.state.oprend)})
-    this.setState({oprend : ""})
   }
 
-  multi = () =>{
-    this.setState({count:(this.state.count*this.state.oprend)})
-    this.setState({oprend : ""})               
+  add = () => {
+    this.setState({ count: parseInt(this.state.count) + parseInt(this.state.oprend) })
+    this.setState({ oprend: "" })
   }
-  div = () =>{
-    this.setState({count:(this.state.count/this.state.oprend)})
-    this.setState({oprend : ""})
+  sub = () => {
+    this.setState({ count: (this.state.count - this.state.oprend) })
+    this.setState({ oprend: "" })
   }
-  render(){
-    return(
+
+  multi = () => {
+    this.setState({ count: (this.state.count * this.state.oprend) })
+    this.setState({ oprend: "" })
+  }
+  div = () => {
+    this.setState({ count: (this.state.count / this.state.oprend) })
+    this.setState({ oprend: "" })
+  }
+  render() {
+    return (
       <div className="App">
         <div className="container">
           <h6>Small Calculator</h6>
-          <h1> {this.state.count} </h1>
-          <input type="text" placeholder="Enter...." value={this.state.oprend} onChange={(e)=>{this.setState({oprend:e.target.value})}}  />
-          <Button id="sub" type="submit" variant="success" onClick={this.sub}>Substraction</Button>
-          <Button id="add" type="submit" variant="primary" onClick={this.add}>Addition</Button>
-          <Button id="multi" type="submit" variant="info" onClick={this.multi}>Multiplication</Button>
-          <Button id="div" type="submit" variant="secondary" onClick={this.div}>Division</Button>
-
+          <h1><h6>Result</h6> {this.state.count} </h1>
+          <div className="input">
+            <input type="text" placeholder="Enter...." value={this.state.oprend} onChange={(e) => { this.setState({ oprend: e.target.value }) }} />
+          </div>
+          <div className="button">
+            <Button id="sub" type="submit" variant="success" onClick={this.sub}>Substraction</Button>
+            <Button id="add" type="submit" variant="primary" onClick={this.add}>Addition</Button>
+            <Button id="multi" type="submit" variant="info" onClick={this.multi}>Multiplication</Button>
+            <Button id="div" type="submit" variant="secondary" onClick={this.div}>Division</Button>
+          </div>
         </div>
       </div>
     )
